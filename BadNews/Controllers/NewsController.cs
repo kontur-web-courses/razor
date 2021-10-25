b9451467a2312b1c92b661ab36fcd8977c801955
KachusovStepan <stepanurfu@gmail.com>
2021-10-25 18:42:06 +0500

    tasks before 2.3

---
+++ BadNews/Controllers/NewsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BadNews.ModelBuilders.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsModelBuilder newsModelBuilder;
+
+        public NewsController(INewsModelBuilder newsModelBuilder)
+        {
+            this.newsModelBuilder = newsModelBuilder;
+        }
+        
+        // GET
+        public IActionResult Index([FromQuery] int pageIndex = 0)
+        {
+            var model = newsModelBuilder.BuildIndexModel(pageIndex, false, null);
+            return View(model);
+        }
+
+        public IActionResult FullArticle([FromRoute] Guid id)
+        {
+            var model = newsModelBuilder.BuildFullArticleModel(id);
+            if (model is null)
+            {
+                return NotFound();
+            }
+            return View(model);
+        }
+    }
+}
--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -1,19 +1,19 @@
 ﻿using BadNews.ModelBuilders.News;
-using BadNews.Models.News;
+// using BadNews.Models.News;
 using BadNews.Repositories.News;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
+// using System;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using System.Web;
 using Serilog;
 
 namespace BadNews
@@ -35,7 +35,10 @@ namespace BadNews
         {
             services.AddSingleton<INewsRepository, NewsRepository>();
             services.AddSingleton<INewsModelBuilder, NewsModelBuilder>();
-            services.AddControllersWithViews();
+            // services.AddControllersWithViews();
+            var mvcBuilder = services.AddControllersWithViews();
+            if (env.IsDevelopment())
+                mvcBuilder.AddRazorRuntimeCompilation();
         }
 
         // В этом методе конфигурируется последовательность обработки HTTP-запроса
@@ -51,20 +54,25 @@ namespace BadNews
             app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
 
 
-            app.Map("/news", newsApp =>
-            {
-                newsApp.Map("/fullarticle", fullArticleApp =>
-                {
-                    fullArticleApp.Run(RenderFullArticlePage);
-                });
+            // app.Map("/news/fullarticle", fullArticleApp =>
+            // {
+            //     fullArticleApp.Run(RenderFullArticlePage);
+            // });
+            
+            // app.Map("/news", newsApp =>
+            // {
+            //     newsApp.Map("/fullarticle", fullArticleApp =>
+            //     {
+            //         fullArticleApp.Run(RenderFullArticlePage);
+            //     });
+            //
+            //     newsApp.Run(RenderIndexPage);
+            // });
 
-                newsApp.Run(RenderIndexPage);
-            });
-
-            app.MapWhen(context => context.Request.Path == "/", rootPathApp =>
-            {
-                rootPathApp.Run(RenderIndexPage);
-            });
+            // app.MapWhen(context => context.Request.Path == "/", rootPathApp =>
+            // {
+            //     rootPathApp.Run(RenderIndexPage);
+            // });
             
             app.UseRouting();
             app.UseEndpoints(endpoints =>
@@ -74,84 +82,85 @@ namespace BadNews
                     controller = "Errors",
                     action = "StatusCode"
                 });
-                endpoints.MapControllerRoute("default", "{controller}/{action}");
+                // endpoints.MapControllerRoute("default", "{controller=News}/{action=Index}");
+                endpoints.MapControllerRoute("default", "{controller=News}/{action=Index}/{id?}");
             });
         }
 
         // Региональные настройки, которые используются при обработке запросов новостей.
-        private static CultureInfo culture = CultureInfo.CreateSpecificCulture("ru-ru");
+        // private static CultureInfo culture = CultureInfo.CreateSpecificCulture("ru-ru");
 
-        private async Task RenderIndexPage(HttpContext context)
-        {
-            // Model Builder достается из DI-контейнера
-            var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
+        // private async Task RenderIndexPage(HttpContext context)
+        // {
+        //     // Model Builder достается из DI-контейнера
+        //     var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
+        //
+        //     // Извлекаются входные параметры запроса
+        //     int.TryParse(context.Request.Query["pageIndex"], out var pageIndex);
+        //
+        //     // Строится модель страницы
+        //     var model = newsModelBuilder.BuildIndexModel(pageIndex, false, null);
+        //
+        //     // Строится HTML для модели
+        //     string pageHtml = BuildIndexPageHtml(model);
+        //
+        //     // Результат записывается в ответ
+        //     await context.Response.WriteAsync(pageHtml);
+        // }
+        //
+        // private static string BuildIndexPageHtml(IndexModel model)
+        // {
+        //     var articlesBuilder = new StringBuilder();
+        //     var articleTemplate = File.ReadAllText("./$Content/Templates/NewsArticle.hbs");
+        //     foreach (var articleModel in model.PageArticles)
+        //     {
+        //         var articleHtml = articleTemplate
+        //             .Replace("{{header}}", articleModel.Header)
+        //             .Replace("{{date}}", articleModel.Date.ToString("d MMM yyyy", culture))
+        //             .Replace("{{teaser}}", articleModel.Teaser)
+        //             .Replace("{{url}}", $"/news/fullarticle/{HttpUtility.UrlEncode(articleModel.Id.ToString())}");
+        //         articlesBuilder.AppendLine(articleHtml);
+        //     }
+        //
+        //     var pageTemplate = File.ReadAllText("./$Content/Templates/Index.hbs");
+        //     var pageHtml = pageTemplate
+        //         .Replace("{{articles}}", articlesBuilder.ToString())
+        //         .Replace("{{newerUrl}}", !model.IsFirst
+        //             ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex - 1).ToString())}"
+        //             : "")
+        //         .Replace("{{olderUrl}}", !model.IsLast
+        //             ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex + 1).ToString())}"
+        //             : "");
+        //     return pageHtml;
+        // }
 
-            // Извлекаются входные параметры запроса
-            int.TryParse(context.Request.Query["pageIndex"], out var pageIndex);
+        // private async Task RenderFullArticlePage(HttpContext context)
+        // {
+        //     // Model Builder достается из DI-контейнера
+        //     var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
+        //
+        //     // Извлекаются входные параметры запроса
+        //     var idString = context.Request.Path.Value.Split('/').ElementAtOrDefault(1);
+        //     Guid.TryParse(idString, out var id);
+        //
+        //     // Строится модель страницы
+        //     var model = newsModelBuilder.BuildFullArticleModel(id);
+        //
+        //     // Строится HTML для модели
+        //     string pageHtml = BuildFullArticlePageHtml(model);
+        //
+        //     // Результат записывается в ответ
+        //     await context.Response.WriteAsync(pageHtml);
+        // }
 
-            // Строится модель страницы
-            var model = newsModelBuilder.BuildIndexModel(pageIndex, false, null);
-
-            // Строится HTML для модели
-            string pageHtml = BuildIndexPageHtml(model);
-
-            // Результат записывается в ответ
-            await context.Response.WriteAsync(pageHtml);
-        }
-
-        private static string BuildIndexPageHtml(IndexModel model)
-        {
-            var articlesBuilder = new StringBuilder();
-            var articleTemplate = File.ReadAllText("./$Content/Templates/NewsArticle.hbs");
-            foreach (var articleModel in model.PageArticles)
-            {
-                var articleHtml = articleTemplate
-                    .Replace("{{header}}", articleModel.Header)
-                    .Replace("{{date}}", articleModel.Date.ToString("d MMM yyyy", culture))
-                    .Replace("{{teaser}}", articleModel.Teaser)
-                    .Replace("{{url}}", $"/news/fullarticle/{HttpUtility.UrlEncode(articleModel.Id.ToString())}");
-                articlesBuilder.AppendLine(articleHtml);
-            }
-
-            var pageTemplate = File.ReadAllText("./$Content/Templates/Index.hbs");
-            var pageHtml = pageTemplate
-                .Replace("{{articles}}", articlesBuilder.ToString())
-                .Replace("{{newerUrl}}", !model.IsFirst
-                    ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex - 1).ToString())}"
-                    : "")
-                .Replace("{{olderUrl}}", !model.IsLast
-                    ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex + 1).ToString())}"
-                    : "");
-            return pageHtml;
-        }
-
-        private async Task RenderFullArticlePage(HttpContext context)
-        {
-            // Model Builder достается из DI-контейнера
-            var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
-
-            // Извлекаются входные параметры запроса
-            var idString = context.Request.Path.Value.Split('/').ElementAtOrDefault(1);
-            Guid.TryParse(idString, out var id);
-
-            // Строится модель страницы
-            var model = newsModelBuilder.BuildFullArticleModel(id);
-
-            // Строится HTML для модели
-            string pageHtml = BuildFullArticlePageHtml(model);
-
-            // Результат записывается в ответ
-            await context.Response.WriteAsync(pageHtml);
-        }
-
-        private static string BuildFullArticlePageHtml(FullArticleModel model)
-        {
-            var pageTemplate = File.ReadAllText("./$Content/Templates/FullArticle.hbs");
-            var pageHtml = pageTemplate
-                .Replace("{{header}}", model.Article.Header)
-                .Replace("{{date}}", model.Article.Date.ToString("d MMM yyyy", culture))
-                .Replace("{{content}}", model.Article.ContentHtml);
-            return pageHtml;
-        }
+        // private static string BuildFullArticlePageHtml(FullArticleModel model)
+        // {
+        //     var pageTemplate = File.ReadAllText("./$Content/Templates/FullArticle.hbs");
+        //     var pageHtml = pageTemplate
+        //         .Replace("{{header}}", model.Article.Header)
+        //         .Replace("{{date}}", model.Article.Date.ToString("d MMM yyyy", culture))
+        //         .Replace("{{content}}", model.Article.ContentHtml);
+        //     return pageHtml;
+        // }
     }
 }
