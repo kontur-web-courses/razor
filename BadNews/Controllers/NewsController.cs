89647317238fe3bddafdce7966ab8031e15bdf6f
Darya <dashav1605@mail.ru>
2021-10-25 19:02:29 +0500

    tasks 1-2.4 done

---
+++ BadNews/Controllers/NewsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BadNews.ModelBuilders.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsModelBuilder newsModelBuilder;
+
+        public NewsController(INewsModelBuilder newsModelBuilder)
+        {
+            this.newsModelBuilder = newsModelBuilder;
+        }
+
+        public IActionResult Index(int pageIndex = 0)
+        {
+            return View(newsModelBuilder.BuildIndexModel(pageIndex, true, null));
+        }
+        
+        public IActionResult FullArticle(Guid id)
+        {
+            var model = newsModelBuilder.BuildFullArticleModel(id);
+            if (model == null)
+                return NotFound();
+            return View(model);
+        }
+    }
+}
