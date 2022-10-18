99b99cbd982cd71f73942cc44fd29f3de0222fc4
Артём <a.burgart02@gmail.com>
2022-10-18 17:46:32 +0500

    2.3

---
+++ BadNews/Controllers/NewsController.cs
@@ -0,0 +1,29 @@
+using BadNews.ModelBuilders.News;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BadNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsModelBuilder newsModelBuilder;
+
+        public NewsController(INewsModelBuilder newsModelBuilder)
+        {
+            this.newsModelBuilder = newsModelBuilder;
+        }
+
+        public IActionResult FullArticle(Guid id)
+        {
+            var model = newsModelBuilder.BuildFullArticleModel(id);
+            if (model == null)
+                return NotFound();
+            return View(model);
+        }
+
+        public IActionResult Index(int pageIndex = 0)
+        {
+            return View(newsModelBuilder.BuildIndexModel(pageIndex, false, null));
+        }
+    }
+}
