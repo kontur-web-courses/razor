05bf0d6d4aa67c3301f75b2fc923027b7dfdff2a
i.krasheninnikov <i.krasheninnikov@skbkontur.ru>
2022-10-18 16:25:10 +0500

    2.3

---
+++ BadNews/Views/Errors/Exception.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BadNews.Views.Errors
+{
+    public class Exception : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
