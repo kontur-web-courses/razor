89b6c436a2e747936445ebd7df8dd55064f5a04e
Egor_Usov <egotronn@gmail.com>
2022-10-19 06:14:21 +0500

    First 2 tasks done

---
+++ BadNews/Controllers/NewsController.cs
@@ -0,0 +1,33 @@
+﻿using BadNews.ModelBuilders.News;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BadNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsModelBuilder newsModelBuilder;
+
+        public NewsController(INewsModelBuilder newsModelBuilder)
+        {
+            this.newsModelBuilder = newsModelBuilder;
+        }
+
+        public IActionResult Index(int pageIndex = 0)
+        {
+            var model = newsModelBuilder.BuildIndexModel(pageIndex, true, null);
+            
+            return View(model);
+        }
+
+        public IActionResult FullArticle(Guid id)
+        {
+            var model = newsModelBuilder.BuildFullArticleModel(id);
+
+            if (model == null)
+                return NotFound();
+
+            return View(model);
+        }
+    }
+}
