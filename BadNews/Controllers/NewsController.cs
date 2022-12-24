32743f2f1defcc77ff48b925b8d7f277af8d88d3
Kirill Novoselov <K.S.Novoselov@urfu.me>
2022-12-24 21:06:02 +0500

    task 2

---
+++ BadNews/Controllers/NewsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BadNews.ModelBuilders.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsModelBuilder newsModelBuilder;
+
+        public NewsController(INewsModelBuilder newsModelBuilder)
+        {
+            this.newsModelBuilder = newsModelBuilder;
+        }
+
+        public IActionResult Index(int pageIndex = 0)
+        {
+            var model = newsModelBuilder.BuildIndexModel(pageIndex, true, null);
+            return View(model);
+        }
+        
+        public IActionResult FullArticle(Guid id)
+        {
+            var model = newsModelBuilder.BuildFullArticleModel(id);
+            return model is null ? NotFound() : View(model);
+        }
+    }
+}
