a3ce4392f65f862dec55e654528417117b6e496a
DESKTOP-FEDOR <pichuginf2016@mail.ru>
2023-11-30 13:13:32 +0500

    1.1

---
+++ BadNews/Controllers/ErrorsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public IActionResult StatusCode(int? code)
+        {
+            return View(code);
+        }
+        
+    }
+}
