0694308873bf77820cbcc1ed3d4db6e74e864ca5
German Markov <german.markov@urfu.me>
2023-12-07 07:18:56 +0500

    Task 2.2 done

---
+++ BadNews/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BadNews.ModelBuilders.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly INewsModelBuilder newsModelBuilder;
+
+        public NewsController(INewsModelBuilder newsModelBuilder)
+        {
+            this.newsModelBuilder = newsModelBuilder;
+        }
+
+        public IActionResult Index(int pageIndex = 0)
+        {
+            return View(newsModelBuilder.BuildIndexModel(pageIndex, false, null));
+        }
+
+        public IActionResult FullArticle(Guid id)
+        {
+            // Строится модель страницы
+            var model = newsModelBuilder.BuildFullArticleModel(id);
+            if (model == null)
+                return NotFound();
+            return View(model);
+        }
+    }
+}
