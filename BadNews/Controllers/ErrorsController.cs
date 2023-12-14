64929a27059dbd1cea4df87f7e1de0901cba66de
Andrey Tolchenov <a.tolchenov@skbkontur.ru>
2023-12-14 12:33:48 +0500

    1.3

--- BadNews/Controllers/ErrorsController.cs
+++ BadNews/Controllers/ErrorsController.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BadNews.Controllers
 {
     public class ErrorsController : Controller
     {
+        private ILogger<ErrorsController> Logger { get; init; }
+        public ErrorsController(ILogger<ErrorsController> logger)
+        {
+            Logger = logger;
+        }
         public IActionResult Exception()
         {
             return View(null, HttpContext.TraceIdentifier);
         }
         public IActionResult StatusCode(int? code)
         {
+            Logger.LogWarning("status-code {code} at {time}", code, DateTime.Now);
             return View(code);
         }
     }
--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Serilog;
 
 namespace BadNews
 {
@@ -44,6 +45,7 @@ namespace BadNews
             else app.UseExceptionHandler("/Errors/Exception");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSerilogRequestLogging();
             app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
 
             app.Map("/news", newsApp =>
