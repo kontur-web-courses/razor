b79ffef12eccd59d779d8d0c7c3e009ac862e04a
Andrey Tolchenov <a.tolchenov@skbkontur.ru>
2023-12-14 13:49:47 +0500

    4.1

---
+++ BadNews/Components/ArchiveLinksViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using BadNews.Repositories.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Components
+{
+    public class ArchiveLinksViewComponent : ViewComponent
+    {
+        private INewsRepository NewsRepository { get; }
+        public ArchiveLinksViewComponent(INewsRepository newsRepository)
+        {
+            NewsRepository = newsRepository;
+        }
+
+        public IViewComponentResult Invoke() => View(NewsRepository.GetYearsWithArticles());
+    }
+}
