ba5d7190f158d3eacb2295b08fc20f741e3d001c
nikunis <nik.unisikhin.05@gmail.com>
2023-12-26 14:12:05 +0500

    done

--- BadNews/Repositories/Weather/WeatherForecastRepository.cs
+++ BadNews/Repositories/Weather/WeatherForecastRepository.cs
@@ -1,17 +1,38 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace BadNews.Repositories.Weather
 {
     public class WeatherForecastRepository : IWeatherForecastRepository
     {
-        private const string defaultWeatherImageUrl = "/images/cloudy.png";
+        private const string defaultImageUrl = "/images/cloudy.png";
 
         private readonly Random random = new Random();
 
+        private readonly OpenWeatherClient client;
+
+        private ILogger<WeatherForecastRepository> log;
+
+        public WeatherForecastRepository(IOptions<OpenWeatherOptions> weatherOptions, ILogger<WeatherForecastRepository> log)
+        {
+            this.log = log;
+            client = new OpenWeatherClient(weatherOptions?.Value.ApiKey);
+        }
+
         public async Task<WeatherForecast> GetWeatherForecastAsync()
         {
-            return BuildRandomForecast();
+            try
+            {
+                var weather = await client.GetWeatherFromApiAsync();
+                return WeatherForecast.CreateFrom(weather);
+            }
+            catch
+            {
+                log.LogWarning("Error get weather");
+                return BuildRandomForecast();
+            }
         }
 
         private WeatherForecast BuildRandomForecast()
@@ -20,7 +41,7 @@ namespace BadNews.Repositories.Weather
             return new WeatherForecast
             {
                 TemperatureInCelsius = temperature,
-                IconUrl = defaultWeatherImageUrl
+                IconUrl = defaultImageUrl
             };
         }
     }
--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -11,9 +11,12 @@ using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
+using BadNews.Elevation;
+using BadNews.Repositories.Weather;
+using BadNews.Validation;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Serilog;
 
 namespace BadNews
 {
@@ -34,107 +37,61 @@ namespace BadNews
         {
             services.AddSingleton<INewsRepository, NewsRepository>();
             services.AddSingleton<INewsModelBuilder, NewsModelBuilder>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, StopWordsAttributeAdapterProvider>();
+            services.AddSingleton<IWeatherForecastRepository, WeatherForecastRepository>();
+            services.Configure<OpenWeatherOptions>(configuration.GetSection("OpenWeather"));
+            services.AddResponseCompression(options => { options.EnableForHttps = true; });
+            services.AddMemoryCache();
+
+
+            var mvcBuilder = services.AddControllersWithViews();
+            if (env.IsDevelopment())
+                mvcBuilder.AddRazorRuntimeCompilation();
         }
 
         // В этом методе конфигурируется последовательность обработки HTTP-запроса
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseExceptionHandler("/Errors/Exception");
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.Map("/news", newsApp =>
+            app.UseResponseCompression();
+            app.UseStaticFiles(new StaticFileOptions()
             {
-                newsApp.Map("/fullarticle", fullArticleApp =>
+                OnPrepareResponse = options =>
                 {
-                    fullArticleApp.Run(RenderFullArticlePage);
+                    options.Context.Response.GetTypedHeaders().CacheControl =
+                        new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+                        {
+                            Public = false,
+                            MaxAge = TimeSpan.FromSeconds(30)
+                        };
+                }
+            });
+            app.UseSerilogRequestLogging();
+            app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
+            app.UseMiddleware<ElevationMiddleware>();
+            
+
+
+            //app.MapWhen(context => context.Request.Path == "/", rootPathApp => { rootPathApp.Run(RenderIndexPage); });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("status-code", "StatusCode/{code?}", new
+                {
+                    controller = "Errors",
+                    action = "StatusCode"
                 });
-
-                newsApp.Run(RenderIndexPage);
+                endpoints.MapControllerRoute("default", "{controller=News}/{action=Index}/{id?}");
             });
-
-            app.MapWhen(context => context.Request.Path == "/", rootPathApp =>
-            {
-                rootPathApp.Run(RenderIndexPage);
-            });
-
+            app.MapWhen(context => context.Request.IsElevated(),
+                branchApp => { branchApp.UseDirectoryBrowser("/files"); });
             // Остальные запросы — 404 Not Found
-        }
-
-        // Региональные настройки, которые используются при обработке запросов новостей.
-        private static CultureInfo culture = CultureInfo.CreateSpecificCulture("ru-ru");
-
-        private async Task RenderIndexPage(HttpContext context)
-        {
-            // Model Builder достается из DI-контейнера
-            var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
-
-            // Извлекаются входные параметры запроса
-            int.TryParse(context.Request.Query["pageIndex"], out var pageIndex);
-
-            // Строится модель страницы
-            var model = newsModelBuilder.BuildIndexModel(pageIndex, false, null);
-
-            // Строится HTML для модели
-            string pageHtml = BuildIndexPageHtml(model);
-
-            // Результат записывается в ответ
-            await context.Response.WriteAsync(pageHtml);
-        }
-
-        private static string BuildIndexPageHtml(IndexModel model)
-        {
-            var articlesBuilder = new StringBuilder();
-            var articleTemplate = File.ReadAllText("./$Content/Templates/NewsArticle.hbs");
-            foreach (var articleModel in model.PageArticles)
-            {
-                var articleHtml = articleTemplate
-                    .Replace("{{header}}", articleModel.Header)
-                    .Replace("{{date}}", articleModel.Date.ToString("d MMM yyyy", culture))
-                    .Replace("{{teaser}}", articleModel.Teaser)
-                    .Replace("{{url}}", $"/news/fullarticle/{HttpUtility.UrlEncode(articleModel.Id.ToString())}");
-                articlesBuilder.AppendLine(articleHtml);
-            }
-
-            var pageTemplate = File.ReadAllText("./$Content/Templates/Index.hbs");
-            var pageHtml = pageTemplate
-                .Replace("{{articles}}", articlesBuilder.ToString())
-                .Replace("{{newerUrl}}", !model.IsFirst
-                    ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex - 1).ToString())}"
-                    : "")
-                .Replace("{{olderUrl}}", !model.IsLast
-                    ? $"/news?pageIndex={HttpUtility.UrlEncode((model.PageIndex + 1).ToString())}"
-                    : "");
-            return pageHtml;
-        }
-
-        private async Task RenderFullArticlePage(HttpContext context)
-        {
-            // Model Builder достается из DI-контейнера
-            var newsModelBuilder = context.RequestServices.GetRequiredService<INewsModelBuilder>();
-
-            // Извлекаются входные параметры запроса
-            var idString = context.Request.Path.Value.Split('/').ElementAtOrDefault(1);
-            Guid.TryParse(idString, out var id);
-
-            // Строится модель страницы
-            var model = newsModelBuilder.BuildFullArticleModel(id);
-
-            // Строится HTML для модели
-            string pageHtml = BuildFullArticlePageHtml(model);
-
-            // Результат записывается в ответ
-            await context.Response.WriteAsync(pageHtml);
-        }
-
-        private static string BuildFullArticlePageHtml(FullArticleModel model)
-        {
-            var pageTemplate = File.ReadAllText("./$Content/Templates/FullArticle.hbs");
-            var pageHtml = pageTemplate
-                .Replace("{{header}}", model.Article.Header)
-                .Replace("{{date}}", model.Article.Date.ToString("d MMM yyyy", culture))
-                .Replace("{{content}}", model.Article.ContentHtml);
-            return pageHtml;
         }
     }
 }
